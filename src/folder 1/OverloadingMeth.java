ee565ab5ad050cdd9767882c83d6ce27a604db59
Rikzapriyudhahirdad <rikza79@gmail.com>
2016-03-21 02:09:34 +0700

    Update OverloadingMeth.java
    maaf kak, berhubung ada kesalahan, program saya ganti

--- src/folder 1/OverloadingMeth.java
+++ src/folder 1/OverloadingMeth.java
@@ -2,115 +2,81 @@ package cek.kata.angka;
 
 public class OverloadingMeth {
 
-    private int angka;
     private String kata;
+    private int angka;
+    String[] ejakata = {"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas"};
 
     public void overloadingmeth(int angka) {
         this.angka = angka;
-        String[] string = {"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas"};
         String hasil;
-        if (angka < 12) {
-            hasil = string[angka];
+        if (angka <= 11) {
+            hasil = ejakata[angka];
             System.out.println(hasil);
         } else if (angka < 20) {
-            hasil = string[angka - 10] + " belas";
+            hasil = ejakata[angka - 10] + " belas";
             System.out.println(hasil);
         } else if (angka < 100) {
-            hasil = string[angka / 10] + " puluh " + string[angka % 10];
+            hasil = ejakata[angka / 10] + " puluh " + ejakata[angka % 10];
             System.out.println(hasil);
         } else if (angka == 100) {
             System.out.println("seratus");
         }
     }
 
-    public void overloadingmeth(String kata) {
-        this.kata = kata;
-        String i[] = new String[1];
-        if (kata.contains(" ")) {
-            kata1(kata);
+    public static void overloadingmeth(String a) {
+        String kata[] = a.split(" ");
+        int ang[] = new int[3];
+        for (int i = 0; i < kata.length; i++) {
+
+            if (i == 1) {
+                continue;
+            }
+            if ((kata[i]).equalsIgnoreCase("satu")) {
+                ang[i] = 1;
+            } else if (kata[i].equalsIgnoreCase("dua")) {
+                ang[i] = 2;
+            } else if (kata[i].equalsIgnoreCase("tiga")) {
+                ang[i] = 3;
+            } else if (kata[i].equalsIgnoreCase("empat")) {
+                ang[i] = 4;
+            } else if (kata[i].equalsIgnoreCase("lima")) {
+                ang[i] = 5;
+            } else if (kata[i].equalsIgnoreCase("enam")) {
+                ang[i] = 6;
+            } else if (kata[i].equalsIgnoreCase("tujuh")) {
+                ang[i] = 7;
+            } else if (kata[i].equalsIgnoreCase("delapan")) {
+                ang[i] = 8;
+            } else if (kata[i].equalsIgnoreCase("sembilan")) {
+                ang[i] = 9;
+            }
+        }
+        if (!a.contains(" ")) {
+
+            if (kata[0].equalsIgnoreCase("sepuluh")) {
+                ang[0] = 10;
+            } else if (kata[0].equalsIgnoreCase("sebelas")) {
+                ang[0] = 11;
+            } else if (kata[0].equalsIgnoreCase("nol")) {
+                ang[0] = 0;
+            } else if (kata[0].equalsIgnoreCase("seratus")) {
+                ang[0] = 100;
+            }
+
+            System.out.println(ang[0]);
+
+        } else if (kata.length == 2) {
+
+            if (kata[1].equalsIgnoreCase("belas")) {
+                ang[1] = 1;
+                System.out.println(ang[1] + "" + ang[0]);
+
+            } else if (kata[1].equalsIgnoreCase("puluh")) {
+                ang[1] = 0;
+                System.out.println(ang[0] + "" + ang[1]);
+            }
         } else {
-            i[0] = kata;
-            if (i[0].equalsIgnoreCase("dua")) {
-                System.out.print("2");
-            } else if (i[0].equalsIgnoreCase("tiga")) {
-                System.out.print("3");
-            } else if (i[0].equalsIgnoreCase("empat")) {
-                System.out.print("4");
-            } else if (i[0].equalsIgnoreCase("lima")) {
-                System.out.print("5");
-            } else if (i[0].equalsIgnoreCase("enam")) {
-                System.out.print("6");
-            } else if (i[0].equalsIgnoreCase("tujuh")) {
-                System.out.print("7");
-            } else if (i[0].equalsIgnoreCase("delapan")) {
-                System.out.print("8");
-            } else if (i[0].equalsIgnoreCase("sembilan")) {
-                System.out.print("9");
-            } else if (i[0].equalsIgnoreCase("satu")) {
-                System.out.print("1");
-            } else if (i[0].equalsIgnoreCase("sepuluh")) {
-                System.out.print("10");
-            } else if (i[0].equalsIgnoreCase("sebelas")) {
-                System.out.print("11");
-            } else if (i[0].equalsIgnoreCase("seratus")) {
-                System.out.println("100");
-            }
-
-        }
-
-    }
-
-    public void kata1(String kata) {
-        this.kata = kata;
-        String A[] = kata.split(" ");
-        if (A[1].equalsIgnoreCase("belas")) {
-            System.out.print("1");
-        }
-        if (A[0].equalsIgnoreCase("dua")) {
-            System.out.print("2");
-        } else if (A[0].equalsIgnoreCase("tiga")) {
-            System.out.print("3");
-        } else if (A[0].equalsIgnoreCase("empat")) {
-            System.out.print("4");
-        } else if (A[0].equalsIgnoreCase("lima")) {
-            System.out.print("5");
-        } else if (A[0].equalsIgnoreCase("enam")) {
-            System.out.print("6");
-        } else if (A[0].equalsIgnoreCase("tujuh")) {
-            System.out.print("7");
-        } else if (A[0].equalsIgnoreCase("delapan")) {
-            System.out.print("8");
-        } else if (A[0].equalsIgnoreCase("sembilan")) {
-            System.out.print("9");
-        } else if (A[0].equalsIgnoreCase("satu")) {
-            System.out.print("1");
-        }
-        if (A[1].equalsIgnoreCase("puluh")) {
-            if (A.length == 2) {
-                System.out.print("0");
-            }
-        }
-        if (A.length == 3) {
-            if (A[2].equalsIgnoreCase("dua")) {
-                System.out.print("2");
-            } else if (A[2].equalsIgnoreCase("satu")) {
-                System.out.print("1");
-            } else if (A[2].equalsIgnoreCase("tiga")) {
-                System.out.print("3");
-            } else if (A[2].equalsIgnoreCase("empat")) {
-                System.out.print("4");
-            } else if (A[2].equalsIgnoreCase("lima")) {
-                System.out.print("5");
-            } else if (A[2].equalsIgnoreCase("enam")) {
-                System.out.print("6");
-            } else if (A[2].equalsIgnoreCase("tujuh")) {
-                System.out.print("7");
-            } else if (A[2].equalsIgnoreCase("delapan")) {
-                System.out.print("8");
-            } else if (A[2].equalsIgnoreCase("sembilan")) {
-                System.out.print("9");
-            }
+            System.out.println(ang[0] + "" + ang[2]);
         }
     }
-
 }
