02042b47c476906e260dc9057c8369e63516e44d
Rikzapriyudhahirdad <rikza79@gmail.com>
2016-03-21 02:11:20 +0700

    Update mainOverloadingMethod.java
    maaf kak, berhubung ada kesalahan, program saya ganti

--- src/folder 1/mainOverloadingMethod.java
+++ src/folder 1/mainOverloadingMethod.java
@@ -2,25 +2,24 @@ package cek.kata.angka;
 
 import java.util.Scanner;
 
+
 public class mainOverloadingMethod {
-
     public static void main(String[] args) {
-        Scanner rheyI = new Scanner(System.in);
-        Scanner rheyS = new Scanner(System.in);
-        OverloadingMeth cek1 = new OverloadingMeth();
-        System.out.println("MASUKKAN BILANGAN ANGKA:   (contoh: 19)");
-        int A = rheyI.nextInt();
-        System.out.print("HASILNYA: ");
-        cek1.overloadingmeth(A);
-        System.out.println("");
-        System.out.println("===================================================");
-        System.out.println("");
-        OverloadingMeth cek2 = new OverloadingMeth();
-        System.out.println("MASUKKAN SEBUTAN ANGKA:    (contoh: sembilan belas)");
-        String a = rheyS.nextLine();
-        System.out.print("HASILNYA: ");
-        cek2.overloadingmeth(a);
+        Scanner inI = new Scanner(System.in);
+        Scanner inS = new Scanner (System.in);
+        OverloadingMeth a= new OverloadingMeth();
+        OverloadingMeth b = new OverloadingMeth();
+        System.out.println("  ANGKA => HURUF BILANGAN dan HURUF BILANGAN => ANGKA   ");
+        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = ");
+        System.out.print("Masukkan angka            : ");
+        int angka = inI.nextInt();
+        System.out.print("HASILNYA:                  ");
+        a.overloadingmeth(angka);
         System.out.println();
-
-    }
+        System.out.print("Masukkan Huruf Bilangan   : ");
+        String huruf = inS.nextLine();
+        System.out.print("HASILNYA:                  ");
+        b.overloadingmeth(huruf);
+        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = ");
+    }  
 }
